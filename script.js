a455446ab75029124fbd8101991fbcbf1d3aa4f5
Ankan <ankantalukdar88@gmail.com>
2023-12-10 22:00:15 +0530

    added css

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('.input-custom');
+
+input.addEventListener('input', function() {
+  input.style.textAlign = 'right'; // Change alignment to right when typing
+});
